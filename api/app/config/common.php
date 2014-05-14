62434ccec961e21c6a36c0e103d32899f88e7dd4
alenslan <lanchangqing322@gmail.com>
2014-05-14 14:17:39 +0800

    增加用户信息接口返回头像补全

--- api/app/config/common.php
+++ api/app/config/common.php
@@ -2,8 +2,8 @@
 
 return [
     'point' => 100,//银币金币兑换比例
-    
-    'imgHost' => 'http://www.lltao.com:83/', //图片服务器HOST
-    
-    'urlHost' => 'http://www.lltao.com:83/', //网站APK服务器HOST
+
+    'imgHost' => 'http://dev_img.lltao.com/', //图片服务器HOST
+
+    'urlHost' => 'http://dev_img.lltao.com/', //网站APK服务器HOST
 ];

f4aa547ef22517dc745edb84bf7bf6df27050b54
alenslan <lanchangqing322@gmail.com>
2014-05-14 17:31:45 +0800

    apk地址补全2

--- api/app/config/common.php
+++ api/app/config/common.php
@@ -3,8 +3,8 @@
 return [
     'point' => 100,//银币金币兑换比例
 
-    'imgHost' => 'http://dev_img.lltao.com/', //图片服务器HOST
+    'imgHost' => 'http://dev_www.lltao.com/', //图片服务器HOST
 
-    'apkHost' => 'http://dev_img.lltao.com/download/app/', //网站APK服务器HOST
+    'apkHost' => 'http://dev_www.lltao.com/download/app/', //网站APK服务器HOST
 
 ];
